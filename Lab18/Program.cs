812b7ddb8b149df2f6f0f6a02779210ed8d8887b
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-22 22:45:03 +0300

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correct = true;
+            Console.Write("Введите текст: "); 
+            string text = Console.ReadLine();
+
+            Stack<string> brace = new Stack<string>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(')
+                {
+                    brace.Push(")");
+                }
+                else if (text[i] == '[')
+                {
+                    brace.Push("]");
+                }
+                else if (text[i] == '{')
+                {
+                    brace.Push("}");
+                }
+                else if (text[i] == ')')
+                {
+                    try
+                    {
+                        if (brace.Peek() == ")")
+                        {
+                            brace.Pop();
+                        }
+                        else
+                        {
+                            correct = false;
+                        }
+                    }
+                    catch (System.InvalidOperationException)
+                    {
+                        correct = false;
+                    }
+                }
+                else if (text[i] == ']')
+                {
+                    try
+                    {
+                        if (brace.Peek() == "]")
+                        {
+                            brace.Pop();
+                        }
+                        else
+                        {
+                            correct = false;
+                        }
+                    }
+                    catch (System.InvalidOperationException)
+                    {
+                        correct = false;
+                    }
+                }
+                else if (text[i] == '}')
+                {
+                    try
+                    {
+                        if (brace.Peek() == "}")
+                        {
+                            brace.Pop();
+                        }
+                        else
+                        {
+                            correct = false;
+                        }
+                    }
+                    catch (System.InvalidOperationException)
+                    {
+                        correct = false;
+                    }
+                }
+            }
+
+            if (brace.Count != 0)
+            {
+                correct = false;
+            }
+
+            if (correct)
+            {
+                Console.WriteLine("Скобки расставлены правильно.");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены неправильно.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
